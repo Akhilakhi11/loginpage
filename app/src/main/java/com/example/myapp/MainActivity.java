5e41c705817ab49e4c1778bc43a922e0839c6cf2
Akhil <akhiljska@gmail.com>
2022-02-17 18:52:36 +0530

    working login screen

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,8 +3,11 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<button> extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
