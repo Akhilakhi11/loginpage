ddb23f6c372c586574f0eff2821f21ab186b2b1a
Akhil <akhiljska@gmail.com>
2022-02-21 18:29:55 +0530

    validation for email

--- app/src/main/java/com/example/myapp/MainActivity.java
+++ app/src/main/java/com/example/myapp/MainActivity.java
@@ -3,15 +3,54 @@ package com.example.myapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity<button> extends AppCompatActivity {
 
 
+    private EditText inputusername, inputpassword;
+    TextView login;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+
+        login=findViewById(R.id.login);
+        inputusername=findViewById(R.id.username);
+        inputpassword=findViewById(R.id.password);
+
+
+
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                CheckCredentials();
+
+            }
+        });
+
+
 }
+
+    private void CheckCredentials() {
+        String email = inputusername.getText().toString().trim();
+
+        String emailPattern = "[a-zA-Z0-9._-]+@[a-z]+\\.+[a-z]+";
+
+// onClick of button perform this simplest code.
+        if (email.matches(emailPattern))
+        {
+            Toast.makeText(getApplicationContext(),email+"valid email address",Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            Toast.makeText(getApplicationContext(),"Invalid email address", Toast.LENGTH_SHORT).show();
+        }
+        }
+    }
+    }
